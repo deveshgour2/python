a65c6f88b4ba00ada3f5e93722e5fa5d34383933
deveshgour2 <deveshgour6gmail.com>
2026-05-25 01:44:42 +0530

    added pattern program folder

--- loop_program/fibnacci.py
+++ loop_program/fibnacci.py
@@ -1,8 +1,11 @@
 n = int(input("enter the number of  terms: "))
 a = 0 
 b = 1
+temo = 0
 print("fibbonacci series: ")
 for i in range(n):
     print(a, end=" ")
-    a, b = b, a + b
+    temp = a
+    a = b 
+    b = temp + b
 
