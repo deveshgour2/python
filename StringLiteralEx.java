451a67eceaa15bf15bb2da2fbfee1475d8c26fd7
AmodhMourya <beastboyindori@gmail.com>
2026-06-10 20:24:31 +0530

    Add files via upload

---
+++ StringLiteralEx.java
@@ -0,0 +1,17 @@
+
+    public class StringLiteralEx {
+
+	public static void main(String[] args) {
+		String s="Amodh Mourya";
+//		String s='jdnfnsd';//Error
+//		String s=sdkjbds; //Error
+		String ss="123";
+		String sss="33.56";
+		System.out.println(s);
+		System.out.println(ss);
+		System.out.println(sss);
+
+	}
+
+}
+
