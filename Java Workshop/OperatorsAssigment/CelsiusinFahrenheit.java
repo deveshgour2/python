6691f64f657cfe57294b0684db6687ed01c105d5
deveshgour2 <167918265+deveshgour2@users.noreply.github.com>
2026-06-11 13:13:09 +0530

    Delete Java Workshop directory

--- Java Workshop/OperatorsAssigment/CelsiusinFahrenheit.java
+++
@@ -1,9 +0,0 @@
-package OperatorsAssigment;
-
-public class CelsiusinFahrenheit {
-	public static void main(String[] args) {
-		float f = 66;
-		float c = (f - 32) * 5 / 9;
-		System.out.println("Celsius: " + c);
-	}
-}
